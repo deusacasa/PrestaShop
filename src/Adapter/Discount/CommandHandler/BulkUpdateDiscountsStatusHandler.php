cfbd24d7f481dcd3c1a6354493959969ebb8f0a8
Boris HERMANS <boris.hermans@prestashop.com>
2025-11-26 11:20:49 +0100

    Use AbstractBulkCommandHandler for bulk update discount status

--- src/Adapter/Discount/CommandHandler/BulkUpdateDiscountsStatusHandler.php
+++ src/Adapter/Discount/CommandHandler/BulkUpdateDiscountsStatusHandler.php
@@ -28,13 +28,18 @@ namespace PrestaShop\PrestaShop\Adapter\Discount\CommandHandler;
 
 use CartRule;
 use PrestaShop\PrestaShop\Core\CommandBus\Attributes\AsCommandHandler;
+use PrestaShop\PrestaShop\Core\Domain\AbstractBulkCommandHandler;
 use PrestaShop\PrestaShop\Core\Domain\Discount\Command\BulkUpdateDiscountsStatusCommand;
 use PrestaShop\PrestaShop\Core\Domain\Discount\CommandHandler\BulkUpdateDiscountsStatusHandlerInterface;
+use PrestaShop\PrestaShop\Core\Domain\Discount\Exception\BulkDiscountException;
 use PrestaShop\PrestaShop\Core\Domain\Discount\Exception\CannotUpdateDiscountException;
+use PrestaShop\PrestaShop\Core\Domain\Discount\Exception\DiscountException;
 use PrestaShop\PrestaShop\Core\Domain\Discount\Exception\DiscountNotFoundException;
+use PrestaShop\PrestaShop\Core\Domain\Discount\ValueObject\DiscountId;
+use PrestaShop\PrestaShop\Core\Domain\Exception\BulkCommandExceptionInterface;
 
 #[AsCommandHandler]
-final class BulkUpdateDiscountsStatusHandler implements BulkUpdateDiscountsStatusHandlerInterface
+final class BulkUpdateDiscountsStatusHandler extends AbstractBulkCommandHandler implements BulkUpdateDiscountsStatusHandlerInterface
 {
     /**
      * {@inheritdoc}
@@ -44,17 +49,45 @@ final class BulkUpdateDiscountsStatusHandler implements BulkUpdateDiscountsStatu
      */
     public function handle(BulkUpdateDiscountsStatusCommand $command): void
     {
-        foreach ($command->getDiscountIds() as $discountId) {
-            $entity = new CartRule($discountId->getValue());
-            $entity->active = $command->getNewStatus();
+        $this->handleBulkAction($command->getDiscountIds(), DiscountException::class, $command);
+    }
 
-            if (!$entity->id) {
-                throw new DiscountNotFoundException(sprintf('Discount with id "%s" was not found', $discountId->getValue()));
-            }
+    /**
+     * @param DiscountId $id
+     * @param BulkUpdateDiscountsStatusCommand $command
+     *
+     * @return void
+     */
+    protected function handleSingleAction(mixed $id, mixed $command): void
+    {
+        $entity = new CartRule($id->getValue());
+        $entity->active = $command->getNewStatus();
 
-            if (!$entity->update()) {
-                throw new CannotUpdateDiscountException(sprintf('Cannot update status for discount with id "%s"', $discountId->getValue()));
-            }
+        if (!$entity->id) {
+            throw new DiscountNotFoundException(sprintf('Discount with id "%s" was not found', $id->getValue()));
         }
+        if (!$entity->update()) {
+            throw new CannotUpdateDiscountException(sprintf('Cannot update status for discount with id "%s"', $id->getValue()));
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function buildBulkException(array $caughtExceptions): BulkCommandExceptionInterface
+    {
+        return new BulkDiscountException(
+            $caughtExceptions,
+            'Errors occurred during discount bulk change status action',
+            BulkDiscountException::FAILED_BULK_UPDATE_STATUS
+        );
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function supports($id): bool
+    {
+        return $id instanceof DiscountId;
     }
 }
